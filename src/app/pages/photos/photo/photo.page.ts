b667faf2635c04fdcd311894a3609d80c6f3e691
Davor Mihajleski <davormihajleski@gmail.com>
2026-04-06 08:24:53 +0200

    fix: adding a toast message as well

--- src/app/pages/photos/photo/photo.page.ts
+++ src/app/pages/photos/photo/photo.page.ts
@@ -5,6 +5,7 @@ import { NgOptimizedImage } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { LoadingComponent } from '@/components/loading/loading.component';
 import { FavoritesService } from '@/services/favorites.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'section.photo',
@@ -13,6 +14,7 @@ import { FavoritesService } from '@/services/favorites.service';
   styleUrl: 'photo.page.scss',
 })
 export class PhotoPage {
+  private snackBar = inject(MatSnackBar);
   private readonly favoritesService = inject(FavoritesService);
   private readonly route = inject(ActivatedRoute);
   photo!: Photo;
@@ -30,6 +32,9 @@ export class PhotoPage {
   removeFromFavorites() {
     if (this.isFavorite()) {
       this.favoritesService.toggle(this.photo);
+      this.snackBar.open('Successfully removed from favorites!');
+    } else {
+      this.snackBar.open('Firts, add this photo to your favorites, so we can remove it!');
     }
   }
 }
